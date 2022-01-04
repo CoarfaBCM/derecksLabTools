99b59c49d3db6213d04a83f263c9d818df8c9dc9
dereckdemezquita <dereck@demezquita.com>
2022-01-04 16:34:18 -0500

    Read gmt, write gmt, convert signatures (EdgeR script) to gmt up down pathways.

---
+++ R/read.gmt.R
@@ -0,0 +1,32 @@
+#' Read a .gmt pathway type file as a list.
+#'
+#' @param file \[type: character\] Path to the file.
+#' @param remove_description \[type: logical, default: FALSE\] Whether to remove description (second element of pathway; first is pathway name) of the pathway.
+#'
+#' @return list<character>
+#' @export
+#'
+#' @examples
+#'
+
+read.gmt <- function(file, remove_description = FALSE) {
+    if(!grepl("\\.gmt$", file)[1]) {
+        stop('Must be a ".gmt" file')
+    }
+
+    geneSetDB <- readLines(file) # read in the gmt file as a vector of lines
+    geneSetDB <- strsplit(geneSetDB, "\t") # convert from vector of strings to a list
+    names(geneSetDB) <- sapply(geneSetDB, "[", 1) # move the names column as the names of the list
+
+    if(remove_description) {
+        geneSetDB <- lapply(geneSetDB, "[", -1:-2) # remove name and description columns
+    } else {
+        geneSetDB <- lapply(geneSetDB, "[", -1) # remove only name column
+    }
+
+    geneSetDB <- lapply(geneSetDB, function(x) {
+        return(x[which(x != "")])
+    }) # remove empty strings
+
+    return(geneSetDB)
+}
---
+++ R/sig2GmtList.R
@@ -0,0 +1,57 @@
+#' Signature(s) files to GMT list; split up and down (inclusive) based on log2fc
+#'
+#' @param signatures_dir \[type: character\] A directory containing signature files.
+#' @param pattern \[type: character<regex>, default: NULL\] A regular expression to match files on read-in.
+#' @param to_upper \[type: logical, default: TRUE\] Convert genes to uppercase; typically yes, lowercase genes are mice type - GSEA works with uppercase humanised genes.
+#'
+#' @return list<character>
+#' @export
+#'
+#' @examples
+#'
+
+# Signatures are split into up and down genes (down inclusive)
+
+sig2UpDownGmt <- function(signatures_dir, pattern = NULL, to_upper = TRUE) {
+    paths <- list.files("./custom-gmt/signatures-all-fc/", full.names = TRUE, pattern = pattern)
+    names(paths) <- list.files("./custom-gmt/signatures-all-fc/", pattern = pattern)
+
+    gmt <- lapply(paths, function(path) { # read all files
+        return(as.data.frame(suppressWarnings(data.table::fread(path))))
+    })
+
+    gmt <- lapply(gmt, function(signature) { # rename columns
+        colnames(signature) <- c("genes", gsub("EdgeR\\.upperquartile_LRT_RUVr_", "", colnames(signature)[2]))
+        return(signature)
+    })
+
+    gmt <- lapply(gmt, function(signature) { # split up and down; concatenate signature name as pathway name with UP/DOWN
+        signature <- as.data.frame(signature)
+        return(list(
+            up = as.character((function(df_arrays) {
+                cbind(
+                    rep(paste(rownames(df_arrays)[2], "UP", sep = "_"), 2),
+                    rep("na", 2),
+                    df_arrays
+                )
+            })(as.data.frame(t(signature[signature[, 2] > 0,])))[1, ]),
+            down = as.character((function(df_arrays) {
+                cbind(
+                    rep(paste(rownames(df_arrays)[2], "DOWN", sep = "_"), 2),
+                    rep("na", 2),
+                    df_arrays
+                )
+            })(as.data.frame(t(signature[signature[, 2] <= 0,])))[1, ])
+        ))
+    })
+
+    if(to_upper) { # convert to uppercase
+        gmt <- lapply(gmt, function(arrs_list) { #
+            return(lapply(arrs_list, function(str_array) {
+                return(c(str_array[1:2], toupper(str_array[3:length(str_array)])))
+            }))
+        })
+    }
+
+    return(unlist(gmt, recursive = FALSE))
+}
